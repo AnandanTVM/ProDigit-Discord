af6c0ebfec6db1341a2a17b36d77cae8ae1e48a5
anandan <anandan2006@gmail.com>
2023-03-19 23:59:54 +0530

    socket compiled

--- Server/app.js
+++ Server/app.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const app = express();
-const cors = require('cors');
-
+const cors = require("cors");
+const { sockets } = require("./socket/socket");
 // data base connection
 const db = require("./config/connection");
 const homeRouter = require("./router/home");
@@ -12,19 +12,17 @@ const { errorHandler, notFound } = require("./middleware/errorMiddleware");
 // .env config
 dotenv.config();
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: process.env.FROUND_END_PORT,
   credentials: true, // access-control-allow-credentials:true
   optionSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
-
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // routs
 app.use("/api", homeRouter);
 app.use("/api/user", userRouter);
-
 
 app.use(errorHandler);
 app.use(notFound);
@@ -35,6 +33,14 @@ db.connect((err) => {
   else console.log("Datebase Connected to port 27017");
 });
 //
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
   console.log(`sever started running on localhost:${process.env.PORT}`);
 });
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: process.env.FROUND_END_PORT,
+  },
+});
+
+io.on("connection", sockets);
